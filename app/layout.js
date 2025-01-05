9a49affc14bf7e528eebd43868730af0ecb5381a
Sanchi Singh <singhsanchi09@gmail.com>
2025-01-05 20:54:59 +0530

    first commit

---
+++ app/layout.js
@@ -0,0 +1,29 @@
+import { Outfit, Ovo} from "next/font/google";
+import "./globals.css";
+
+const Outfit = Outfit({
+  variable: "--font-geist-sans",
+  subsets: ["latin"],
+});
+
+const geistMono = Geist_Mono({
+  variable: "--font-geist-mono",
+  subsets: ["latin"],
+});
+
+export const metadata = {
+  title: "Portfolio",
+  description: "",
+};
+
+export default function RootLayout({ children }) {
+  return (
+    <html lang="en">
+      <body
+        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+      >
+        {children}
+      </body>
+    </html>
+  );
+}
